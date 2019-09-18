c78dfc74af12bafe964d4f06398ae030f06c8398
Jeyson0608 <55483861+Jeyson0608@users.noreply.github.com>
2019-09-18 10:46:36 +0200

    Delete bonjour.py

--- bonjour.py
+++
@@ -1,9 +0,0 @@
-""" bienvenue
-données : le semestre d'entrés
-résultat : affiche un message de bienvenue aux étudiant-e-s
-"""
-### déclaration des varaiables
-### initialisation des variables
-### séquence d'opérations
-print("Bonjour !")
-print("Bienvenue aux étudiant-e-s 2019-2020 !")
